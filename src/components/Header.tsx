d41fcc9a3120a230e72d4ebfc6258377a9f5c677
funnytam1019 <tam.nguyen.10119@gmail.com>
2022-12-20 20:02:43 +0700

    update: adding globals styling

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <nav>
+      <img></img>
+      <ul>
+        <li>Pricing</li>
+        <li>Product</li>
+        <li>About Us</li>
+        <li>Careers</li>
+        <li>Community</li>
+      </ul>
+      <a href="#" className="button">
+        Get Started
+      </a>
+    </nav>
+  );
+};
+export default Header;
