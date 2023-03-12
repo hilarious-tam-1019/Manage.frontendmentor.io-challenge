b1b8c1e460d7d6eb59c4ff0610d5ef375aba35e9
funnytam1019 <tam.nguyen.10119@gmail.com>
2023-03-12 23:07:08 +0700

    update: rebuilding base

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,3 @@
-import Header from './components/header/Header';
-import Hero from './components/hero/Hero';
-import Features from './components/features/Features';
-import Carousel from './components/carousel/Carousel';
-import CTA from './components/cta/CTA';
-import Footer from './components/footer/Footer';
-
-function App() {
-  return (
-    <>
-      <Header></Header>
-      <main>
-        <Hero></Hero>
-        <Features></Features>
-        <Carousel></Carousel>
-        <CTA></CTA>
-        <Footer></Footer>
-      </main>
-    </>
-  );
-}
-
-export default App;
+export const App = () => {
+  return <></>;
+};
